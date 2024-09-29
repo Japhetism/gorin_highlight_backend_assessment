027b6daaae87513f2d3b767f3591868e243d0e81
Babatunde Ojo <babatundeojo30@gmail.com>
2024-09-29 15:53:11 +0100

    format response message

--- src/highlights/highlight.interface.ts
+++ src/highlights/highlight.interface.ts
@@ -2,3 +2,8 @@ export interface IHighlight {
     id: number;
     name: string;
 }
+
+export interface IHighlightResponse {
+    message: string;
+    data: IHighlight | IHighlight[];
+}
--- src/highlights/highlights.controller.ts
+++ src/highlights/highlights.controller.ts
@@ -1,33 +1,53 @@
 import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
 import { HighlightsService } from './highlights.service';
-import { IHighlight } from './highlight.interface';
+import { IHighlight, IHighlightResponse } from './highlight.interface';
 
 @Controller('highlights')
 export class HighlightsController {
   constructor(private readonly highlightsService: HighlightsService) {}
 
   @Post()
-  create(@Body() highlight: Omit<IHighlight, 'id'>): IHighlight {
-    return this.highlightsService.create(highlight);
+  create(@Body() highlight: Omit<IHighlight, 'id'>): IHighlightResponse {
+    const createdHighlight = this.highlightsService.create(highlight);
+    return {
+        message: 'successful',
+        data: createdHighlight
+    }
   }
 
   @Get()
-  findAll(): IHighlight[] {
-    return this.highlightsService.findAll();
+  findAll(): IHighlightResponse {
+    const highlights = this.highlightsService.findAll();
+    return {
+        message: 'successful',
+        data: highlights
+    }
   }
 
   @Get(':id')
-  findOne(@Param('id') id: number): IHighlight {
-    return this.highlightsService.findOne(id);
+  findOne(@Param('id') id: number): IHighlightResponse {
+    const highlight = this.highlightsService.findOne(id);
+    return {
+        message: 'successful',
+        data: highlight
+    }
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() highlight: Partial<IHighlight>): IHighlight {
-    return this.highlightsService.update(id, highlight);
+  update(@Param('id') id: number, @Body() highlight: Partial<IHighlight>): IHighlightResponse {
+    const updatedHighlight = this.highlightsService.update(id, highlight);
+    return {
+        message: 'successful',
+        data: updatedHighlight
+    }
   }
 
   @Delete(':id')
-  delete(@Param('id') id: number): void {
+  delete(@Param('id') id: number): IHighlightResponse {
     this.highlightsService.delete(id);
+    return {
+        message: 'successful',
+        data: null
+    }
   }
 }
